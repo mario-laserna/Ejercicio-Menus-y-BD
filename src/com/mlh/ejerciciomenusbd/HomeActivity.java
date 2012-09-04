c5448dcab5d06d33bbed2049422fb5068c2f0b2b
mario <laserna.mario@gmail.com>
2012-09-03 23:19:18 -0500

    import inicial

---
+++ src/com/mlh/ejerciciomenusbd/HomeActivity.java
@@ -0,0 +1,24 @@
+package com.mlh.ejerciciomenusbd;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_home, menu);
+        return true;
+    }
+
+    
+}
